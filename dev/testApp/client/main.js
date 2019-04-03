9f58e3cc84c3097c891b9db39b15c9163c8dbfa5
GDelavy <gislaind@hotmail.com>
2019-04-03 14:30:10 +0200

    Ajout page utilisateur
    Squelette page utilisateur

--- dev/testApp/client/main.js
+++ dev/testApp/client/main.js
@@ -2,3 +2,19 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+
+console.log("Essai de la console");
+
+	Template.base.helpers({
+		test()
+		{
+			return "Test";
+		}
+		
+		
+	});
+	
+	Template.base.events({
+		
+		
+	});
