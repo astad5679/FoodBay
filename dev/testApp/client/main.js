b4561ba7e815a4a1d2f2ebebbc1344aa5fd1ba69
Andres Stadelmann <astadel5679@gmail.com>
2019-04-08 00:00:05 +0200

    Prototype page "Post"
    Added post html and some template functionalities, namely database entry creation by the server (with no conditions)

--- dev/testApp/client/main.js
+++ dev/testApp/client/main.js
@@ -5,14 +5,15 @@ import './main.html';
 
 
 //Iron:Router
+Router.route('/post');
 Router.route('/user');
 Router.route('/', {
-    template: 'home'
+	template: 'home'
 });
 
 Router.configure({
 	layoutTemplate: 'navBar'
-	
+
 });
 
 
@@ -21,28 +22,52 @@ Repas = new Mongo.Collection('repas');
 
 
 //Méthodes côté client
-if (Meteor.isClient)
-{
-console.log("Essai de la console");
+if (Meteor.isClient) {
+	console.log("Essai de la console");
 
 	Template.home.helpers({
-		test()
-		{
+		test() {
 			return "Test";
 		}
-		
-		
+
+
 	});
-	
+
 	Template.home.events({
-		
-		
+
+
 	});
-	
+
+	Template.post.events({
+		'submit form': function (event) {
+			event.preventDefault();
+			var platNom = event.target.plat.value;
+			var platPortions = event.target.portions.value;
+			var platIngredients =  event.target.ingredients.value;
+			var platCout = event.target.cost.value;
+			var platImage = event.target.image.value;
+
+			Meteor.call('insertRepas', (platNom, platPortions, platIngredients, platCout, platImage));
+			console.log("Nouveau repas");
+		},
+	})
 }
 
 //Méthodes côté serveur
-if (Meteor.isServer)
-{
+if (Meteor.isServer) {
 
+	Meteor.methods({
+		// Methode qui crée une entrée repas a chaque fois que le bouton "Submit" est cliqué sur la page "Post"
+		'insertRepas': function(platNom, platPortions, platIngredients, platCout, platImage) {
+			var currentUserId = Meteor.userId();
+			Repas.insert({
+				nom: platNom,
+				portions: platPortions,
+				ingredients: platIngredients,
+				cout: platCout,
+				image: platImage,
+				createdBy: currentUserId
+			});
+		}
+	});
 }
